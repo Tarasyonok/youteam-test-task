cd55caf10f3700d88531ae37d88552ab7e718b41
Tarasyonok <kir_tarasov@mail.ru>
2025-03-02 16:14:03 +0300

    feat: Add database and configuration

---
+++ app/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import NullPool
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import async_sessionmaker
+
+from app.config import settings
+
+DATABASE_URL = settings.DATABASE_URL
+engine = create_async_engine(DATABASE_URL)
+
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
